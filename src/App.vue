a1e1b77d4ac1a6d9a56c61b32dca6a7ca98bbcc3
Daniil <daniladoljaskiy@gmail.com>
2020-12-20 22:02:53 +0300

    HewPages

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
+
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/login">Login</router-link> |
+    <router-link to="/passwordrecovery">Recovery</router-link>
   </div>
+
   <router-view/>
+
 </template>
 
 <style>
---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>It's login page</h1>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Login'
+}
+</script>
